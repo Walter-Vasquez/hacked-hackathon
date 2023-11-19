ecb9d2dc3945241500e85beb1641bb12870b5122
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-11-18 17:50:13 -0700

    Use dotenv to place variables

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const { PORT } = require("./utilities/config");
 
 const app = express();
-const port = 3003;
+app.use(cors());
 
-app.listen(port, () => {
-  console.log("Server is on port ", port);
+app.listen(PORT, () => {
+  console.log("Server is on port ", PORT);
 });
