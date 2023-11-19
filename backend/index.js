56b23599c93c514d31f5550b59adb56d393c02db
Walter-Vasquez <122485903+Walter-Vasquez@users.noreply.github.com>
2023-11-19 08:00:34 -0700

    update index.js to include ingredientsRouter

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const { PORT } = require("./utilities/config");
 const recipeRouter = require("./controllers/recipes");
+const ingredientsRouter = require("./controllers/ingredients");
 
 const app = express();
 app.use(cors());
 app.use("/api/recipes", recipeRouter);
+app.use("/api/ingredients", ingredientsRouter);
 
 app.listen(PORT, () => {
   console.log("===========Server is running on port ", PORT);
