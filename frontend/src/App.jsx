e3dae6209b08fb12c09dee5fac330b8cb01e5aeb
Sameer Ojha <122489814+autom101@users.noreply.github.com>
2023-11-19 08:39:30 -0700

    Connect ingredients list to the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { IngredientsList } from "./components";
 
 function App() {
+  const ingredients = [{ greeting: "hi" }];
   return (
     <Router>
+      <h1>Now We're Cookin'</h1>
       <Routes>
-        <Route path="/" element={<p>Check if router works</p>}></Route>
+        <Route
+          path="/"
+          element={<IngredientsList ingredients={ingredients} />}
+        ></Route>
       </Routes>
     </Router>
   );
